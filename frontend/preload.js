ee2841de60fc01bc63e374ec112cd3ead2a54c21
Raphael Schuhmann <raphael221@outlook.de>
2026-01-30 09:42:32 +0100

    Removed electron from frontend project

--- frontend/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("api", {
-   openFileDialog: (options) => ipcRenderer.invoke("open-file-dialog", options),
-   readFile: (path) => ipcRenderer.invoke("read-file", path)
-});
