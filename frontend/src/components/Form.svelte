8617e857ac559b143cdd562d0f979a56f65e857e
Raphael Schuhmann <raphael221@outlook.de>
2026-01-30 21:44:58 +0100

    Refactored Form.svelte to take a width and use widthMap and heightMap

--- frontend/src/components/Form.svelte
+++ frontend/src/components/Form.svelte
@@ -1,9 +1,11 @@
 <script>
     import { paddingMap } from "../lib/dynamicStyles";
+    import { widthMap, heightMap } from "../lib/dynamicStyles";
 
     export let height = "3/5";
+    export let width = "1/3";
     export let padding = "5";
 </script>
-<div class={`w-1/3 h-${height} rounded-1 bg-white border-2 border-gv-border flex flex-col items-center ${paddingMap[padding]}`}>
+<div class={`${widthMap[width]} ${heightMap[height]} rounded-1 bg-white border-2 border-gv-border flex flex-col items-center ${paddingMap[padding]}`}>
     <slot />
 </div>
