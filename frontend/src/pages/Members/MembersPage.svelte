259965ba2dbc3d4db3af0c2b3758ed7977d26ebc
Raphael Schuhmann <raphael221@outlook.de>
2026-02-01 13:15:20 +0100

    Refactored Members page to have a mobile ready version

---
+++ frontend/src/pages/Members/MembersPage.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { useViewport } from "../../stores/viewport.svelte";
+    import MembersDesktop from "./MembersDesktop.svelte";
+    import MembersMobile from "./MembersMobile.svelte";
+</script>
+
+{#if useViewport().isMobile}
+    <MembersMobile />
+{:else}
+    <MembersDesktop />
+{/if}
