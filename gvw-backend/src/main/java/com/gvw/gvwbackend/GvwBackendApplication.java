a98c6ceec80397f1118f55ca2fa9083ffe4e2fe9
Raphael Schuhmann <raphael221@outlook.de>
2026-03-14 14:11:54 +0100

    Created new java backend project

---
+++ gvw-backend/src/main/java/com/gvw/gvwbackend/GvwBackendApplication.java
@@ -0,0 +1,12 @@
+package com.gvw.gvwbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvwBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GvwBackendApplication.class, args);
+  }
+}
