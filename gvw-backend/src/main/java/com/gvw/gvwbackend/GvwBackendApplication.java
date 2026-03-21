4fe05e0cdc8be3ee50a09642d7b9c66f439ef24d
Raphael Schuhmann <raphael221@outlook.de>
2026-03-21 23:49:12 +0100

    Updated gitignore, Added custom fixed port

--- gvw-backend/src/main/java/com/gvw/gvwbackend/GvwBackendApplication.java
+++ gvw-backend/src/main/java/com/gvw/gvwbackend/GvwBackendApplication.java
@@ -3,6 +3,8 @@ package com.gvw.gvwbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Map;
+
 @SpringBootApplication(excludeName = {
         "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
         "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
@@ -10,6 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GvwBackendApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(GvwBackendApplication.class, args);
+    SpringApplication app = new SpringApplication(GvwBackendApplication.class);
+
+    app.setDefaultProperties(Map.of("server.port", 3500));
+
+    app.run(args);
   }
 }
