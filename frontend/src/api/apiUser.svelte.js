05d7cf9035badf440dfaae9ce7a898960c4d2c0e
Raphael Schuhmann <raphael221@outlook.de>
2026-04-05 00:28:04 +0200

    Removed update user data functionality

--- frontend/src/api/apiUser.svelte.js
+++ frontend/src/api/apiUser.svelte.js
@@ -30,34 +30,3 @@ export async function getUserData() {
     const body = await parseBodySafe(resp);
     return { resp, body };
 }
-
-/**
- * Attempts to update the user data and its associated member data.
- * 
- * Sends a PATCH request to `/user/update` endpoint with an Authorization header.
- * The server identifies the user from the auth token.
- * 
- * @param {Object} data - the updated user data.
- * @param {string} data.email - the user's email address
- * @param {string} data.phone - the user's phone number
- * @param {string} data.address - the user's address
- * @returns {Promise<{ resp: Response | null, body: any | null }>}
- * An object containing the raw fetch Response (`resp`) and the parsed
- * JSON response body (`body`). Returns `{ resp: null, body: null }`
- * if the request fails before a response is received.
- * 
- * The response body may include an ok flag which can be discarded.
- * 
- * @example
- * const { resp } = await updateUserData();
- * 
- * if (resp?.ok) {
- *   console.log("Data updated");
- * }
- */
-export async function updateUserData(data) {
-    const resp = await httpPatch(`${apiUrl}/user/update`, data);
-    if (!resp) return { resp: null, body: null };
-    const body = await parseBodySafe(resp);
-    return { resp, body };
-}
